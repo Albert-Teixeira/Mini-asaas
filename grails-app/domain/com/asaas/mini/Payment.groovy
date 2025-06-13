50f552889eb5b56623691bd362324e1673c627e3
rafaelalmeida9 <rafaeldealmeida@usp.br>
2025-06-13 11:41:11 -0300

    adicionando mais imports explicitamente

--- grails-app/domain/com/asaas/mini/Payment.groovy
+++ grails-app/domain/com/asaas/mini/Payment.groovy
@@ -3,6 +3,8 @@ package com.asaas.mini
 import com.asaas.mini.utils.BaseEntity
 import com.asaas.mini.PaymentType
 import com.asaas.mini.StatusType
+import com.asaas.mini.Customer
+import com.asaas.mini.Payer
 
 class Payment extends BaseEntity{
 
