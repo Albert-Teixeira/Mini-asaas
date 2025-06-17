d5b3a373f15eba5c4a60850c4dd09aea1160b307
rafaelalmeida9 <rafaeldealmeida@usp.br>
2025-06-17 12:36:30 -0300

    removido bootstrap

--- grails-app/init/com/asaas/mini/BootStrap.groovy
+++ grails-app/init/com/asaas/mini/BootStrap.groovy
@@ -3,57 +3,6 @@ package com.asaas.mini
 class BootStrap {
 
     def init = { servletContext ->
-
-        //Hardcodando alguns dados para posteriores usos
-
-        def customer = new Customer(
-            name: "Carlos",
-            email: "email@email.com",
-            phoneNumber: "15999999999",
-            cpfCnpj: "123.456.789-09",
-            state: "SP",
-            city: "Boituva",
-            street: "Pq. Ecologico",
-            houseNumber: 123,
-            postalCode: "15123-456")
-        
-        customer.save()
-
-        def payer = new Payer(
-            customer: customer,
-            name: "Rodrigo",
-            email: "email2@email.com",
-            phoneNumber: "15999999999",
-            cpfCnpj: "123.456.789-09",
-            state: "SP",
-            city: "Boituva",
-            street: "Pq. Ecologico",
-            houseNumber: 123,
-            postalCode: "15123-456")
-        
-        payer.save()
-        
-        def payment = new Payment(
-            customer: customer,
-            payer: payer,
-            paymentType: PaymentType.BOLETO,
-            value: 45.55,
-            status: StatusType.PENDENTE,
-            dueDate: new Date(),
-            dateReceived: new Date())
-        
-        payment.save()
-
-        payment = new Payment(
-            customer: customer,
-            payer: payer,
-            paymentType: PaymentType.PIX,
-            value: 250,
-            status: StatusType.PENDENTE,
-            dueDate: new Date(),
-            dateReceived: new Date())
-        
-        payment.save()
     }
     def destroy = {
     }
