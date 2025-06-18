291fbc8473203cd40a9e97fbcb619f0bf6969304
rafaelalmeida9 <rafaeldealmeida@usp.br>
2025-06-18 09:57:34 -0300

    adicionado plugin spring security e usuario teste

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,5 @@
+import com.asaas.mini.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener)
 }
--- grails-app/init/com/asaas/mini/BootStrap.groovy
+++ grails-app/init/com/asaas/mini/BootStrap.groovy
@@ -1,9 +1,31 @@
 package com.asaas.mini
 
+import grails.gorm.transactions.Transactional
+
 class BootStrap {
 
-    def init = { servletContext ->
+    def init = {
+        addTestUser()
     }
+
+    @Transactional
+    void addTestUser() {
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save()
+
+        def testUser = new User(username: 'me', password: 'password').save()
+
+        UserRole.create testUser, adminRole
+
+        UserRole.withSession {
+            it.flush()
+            it.clear()
+        }
+
+        assert User.count() == 1
+        assert Role.count() == 1
+        assert UserRole.count() == 1
+    }
+
     def destroy = {
     }
 }
