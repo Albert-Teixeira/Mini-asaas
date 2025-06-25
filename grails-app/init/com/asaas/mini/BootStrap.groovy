1681c70ce21006f29f1f7cff0ca1987aaf16c644
rafaelalmeida9 <rafaeldealmeida@usp.br>
2025-06-25 11:17:42 -0300

    bootstrap removido

--- grails-app/init/com/asaas/mini/BootStrap.groovy
+++ grails-app/init/com/asaas/mini/BootStrap.groovy
@@ -3,17 +3,6 @@ package com.asaas.mini
 class BootStrap {
 
     def init = { servletContext ->
-        def customer = new Customer(
-            name: "Carlos",
-            email: "email@email.com",
-            cpfCnpj: "123.456.789-09",
-            state: "SP",
-            city: "Boituva",
-            street: "Pq. Ecologico",
-            addressNumber: 123,
-            postalCode: "15123-456")
-        
-        customer.save()
     }
     def destroy = {
     }
