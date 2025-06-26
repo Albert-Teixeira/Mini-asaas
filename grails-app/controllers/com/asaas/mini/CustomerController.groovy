cddc6061c754194e76842fc5a896cb11d7c329d7
rafaelalmeida9 <rafaeldealmeida@usp.br>
2025-06-26 10:01:28 -0300

    removido deadcode

--- grails-app/controllers/com/asaas/mini/CustomerController.groovy
+++ grails-app/controllers/com/asaas/mini/CustomerController.groovy
@@ -10,16 +10,6 @@ class CustomerController {
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
-    // @Secured(['ROLE_USER','ROLE_OWNER'])
-    // def index(Integer max) {
-    //     User user = getAuthenticatedUser()
-    //     Customer customer = user.customer
-        
-    //     params.max = Math.min(max ?: 10, 100)
-    //     respond customerService.list(params), model:[customerCount: customerService.count()]
-    // }
-
-    //protegi
     def index() {
         User user = getAuthenticatedUser()
         Customer customer = user.customer
@@ -27,34 +17,28 @@ class CustomerController {
     }
 
     //vai sair
-    def create() {
-        respond new Customer(params)
-    }
+    // def save() {
+    //     try{
+    //         Customer customer = customerService.save(params)
 
-    //vai sair
-    def save() {
-        try{
-            Customer customer = customerService.save(params)
+    //         request.withFormat {
+    //             form multipartForm {
+    //                 flash.message = message(code: 'default.created.message', args: [message(code: 'customer.label', default: 'Customer'), customer.id])
+    //                 redirect customer
+    //             }
+    //             '*' { respond customer, [status: CREATED] }
+    //         }
+    //     } catch (ValidationException e) {
+    //         flash.message = "Erro de validação: ${e.message}"
+    //         respond customer, view:'create'
+    //         return
+    //     } catch (Exception e) {
+    //         flash.message = "Erro ao salvar cliente: ${e.message}"
+    //         respond customer, view:'create'
+    //         return
+    //     }
+    // }
 
-            request.withFormat {
-                form multipartForm {
-                    flash.message = message(code: 'default.created.message', args: [message(code: 'customer.label', default: 'Customer'), customer.id])
-                    redirect customer
-                }
-                '*' { respond customer, [status: CREATED] }
-            }
-        } catch (ValidationException e) {
-            flash.message = "Erro de validação: ${e.message}"
-            respond customer, view:'create'
-            return
-        } catch (Exception e) {
-            flash.message = "Erro ao salvar cliente: ${e.message}"
-            respond customer, view:'create'
-            return
-        }
-    }
-
-    //protegi
     def edit() {
         User user = getAuthenticatedUser()
         Customer customer = user.customer
@@ -62,8 +46,6 @@ class CustomerController {
         respond customer
     }
 
-
-    //protegi
     def update() {
         
         User user = getAuthenticatedUser()
@@ -86,42 +68,6 @@ class CustomerController {
             flash.message = "Erro ao atualizar cliente: ${e.message}"
             respond customer, view:'edit'
             return
-        }
-    }
-
-    //vai sair
-    def delete(Long id) {
-        if (id == null) {
-            notFound()
-            return
-        }
-
-        try{
-            customerService.delete(id)
-
-            request.withFormat {
-                form multipartForm {
-                    flash.message = message(code: 'default.deleted.message', args: [message(code: 'customer.label', default: 'Customer'), id])
-                    redirect action:"index", method:"GET"
-                }
-                '*'{ render status: NO_CONTENT }
-            }
-        } catch (Exception e) {
-            flash.message = e.message
-            request.withFormat {
-                form multipartForm {
-                    redirect action: "index", method: "GET"
-                }
-                '*'{ render status: NOT_FOUND }
-            }
-        } catch (IllegalArgumentException e) {
-            flash.message = "Invalid ID: ${e.message}"
-            request.withFormat {
-                form multipartForm {
-                    redirect action: "index", method: "GET"
-                }
-                '*'{ render status: BAD_REQUEST }
-            }
         }
     }
 
