821ecdc6f419753d9b4023edfbc6dd878719a690
rafaelalmeida9 <rafaeldealmeida@usp.br>
2025-06-27 13:24:08 -0300

    bootstrap atualizado

--- grails-app/init/com/asaas/mini/BootStrap.groovy
+++ grails-app/init/com/asaas/mini/BootStrap.groovy
@@ -5,54 +5,15 @@ import grails.gorm.transactions.Transactional
 class BootStrap {
 
     def init = {
-        addTestUser()
+        initializeRoles()
     }
 
     @Transactional
-    void addTestUser() {
-        // def ownerRole = new Role(authority: 'ROLE_OWNER').save(failOnError: true)
-        // def userRole = new Role(authority: 'ROLE_USER').save(failOnError: true)
-
-        // def customer1 = new Customer(
-        //     name: "Carlos",
-        //     email: "email@email.com",
-        //     phoneNumber: "15999999999",
-        //     cpfCnpj: "123.456.789-09",
-        //     state: "SP",
-        //     city: "Boituva",
-        //     street: "Pq. Ecologico",
-        //     addressNumber: 123,
-        //     postalCode: "15123-456").save(failOnError: true)
-        
-        // def customer2 = new Customer(
-        //     name: "Dono",
-        //     email: "email3@email.com",
-        //     phoneNumber: "99999999999",
-        //     cpfCnpj: "999.999.999-99",
-        //     state: "SP",
-        //     city: "São Carlos",
-        //     street: "Rua",
-        //     addressNumber: 999,
-        //     postalCode: "15999-999").save(failOnError: true)
-
-        // def user1 = new User(username: 'user1', password: '123', customer: customer1).save(failOnError: true)
-        // def owner1 = new User(username: 'owner1', password: '123', customer: customer1).save(failOnError: true)
-        // def user2 = new User(username: 'user2', password: '123', customer: customer2).save(failOnError: true)
-        // def owner2 = new User(username: 'owner2', password: '123', customer: customer2).save(failOnError: true)
-
-        // UserRole.create user1, userRole
-        // UserRole.create user2, userRole
-        // UserRole.create owner1, ownerRole
-        // UserRole.create owner2, ownerRole
-
-        // UserRole.withSession {
-        //     it.flush()
-        //     it.clear()
-        // }
-
-        // assert User.count() == 4
-        // assert Role.count() == 2
-        // assert UserRole.count() == 4
+    void initializeRoles() {
+        if(Role.getAll().size() != 2){
+            Role ownerRole = new Role(authority: 'ROLE_OWNER').save(failOnError: true)
+            Role userRole = new Role(authority: 'ROLE_USER').save(failOnError: true)
+        }
     }
 
     def destroy = {
